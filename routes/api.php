e5877328ad84da8b35bdd4763698217064681d1c
Mochamad Dwi Febriansyah <iniakunnyaorangjoos@gmail.com>
2024-03-30 17:35:00 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProdukController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -14,5 +15,14 @@ Route::group(['prefix' => 'v1/auth'], function() {
 
     Route::group(['middleware' => 'checktoken'], function() {
         Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
+
+Route::group(['prefix' => 'v1'], function() {
+    Route::group(['middleware' => 'checktoken'], function() {
+        Route::get('produk', [ProdukController::class, 'getProduk']);
+        Route::post('produk', [ProdukController::class, 'createProduk']);
+        Route::put('produk/{id}', [ProdukController::class, 'editProduk']);
+        Route::delete('produk/{id}', [ProdukController::class, 'deleteProduk']);
     });
 });
